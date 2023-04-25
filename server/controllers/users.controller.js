e19f0869a3c7df2c60ca3c043a0d2d950209821f
Hoa Huu Nguyen <hoaboy89@gmail.com>
2023-04-25 22:41:11 +0700

    Delete, update, create user [v].

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -67,7 +67,7 @@ const usersController = {
       .status(200)
       .send('User has been logged out.');
   },
-  handleGetAllUsers: async (req, res) => {
+  handleGetAllUsers: async (req, res, next) => {
     let id = req.query.id;
     let users = [];
     if (!id) {
@@ -96,7 +96,7 @@ const usersController = {
     res.send({ status: 200 });
   },
 
-  handleCreateNewUser: async (req, res) => {
+  handleCreateNewUser: async (req, res, next) => {
     if (!req.body) {
       return res.status(200).json({
         errCode: 1,
@@ -105,14 +105,81 @@ const usersController = {
     }
     try {
       let checkEmail = await User.find({ email: req.body.email });
-      console.log(checkEmail);
-      res.send(checkEmail.length === 0);
+      if (checkEmail.length > 0) {
+        return res.status(200).json({
+          errCode: 1,
+          errMessage: 'Email is exit!',
+        });
+      }
+      const hash = bcrypt.hashSync(req.body.password, 5);
+      const newUser = new User({
+        ...req.body,
+        password: hash,
+      });
+      await newUser.save();
+      res.status(201).json({
+        errCode: 0,
+        errMessage: 'Create user success!',
+      });
     } catch (error) {
       next(createError(500, 'Some thing went wrong!'));
     }
   },
-  handleEditUser: () => {},
-  handleDeleteUser: () => {},
+  handleEditUser: async (req, res, next) => {
+    const data = req.body;
+    if (!data.id || !data.roleId || !data.positionId || !data.gender) {
+      return res
+        .status(200)
+        .json({ errCode: 2, errMessage: 'Missing parameters' });
+    }
+    if (data.password) {
+      return res
+        .status(200)
+        .json({ errCode: 2, errMessage: 'Can not update.' });
+    }
+    try {
+      const userUpdate = await User.findById(data.id);
+      if (userUpdate) {
+        userUpdate.roleId = data.roleId;
+        userUpdate.positionId = data.positionId;
+        userUpdate.gender = data.gender;
+        await userUpdate.save();
+        res.status(200).json({
+          errCode: 0,
+          message: 'Update the user succeeds!',
+        });
+      }
+    } catch (error) {
+      next(createError(500, 'Some thing went wrong!'));
+    }
+  },
+  handleDeleteUser: async (req, res, next) => {
+    if (!req.body.id) {
+      return res.status(200).json({
+        errCode: 1,
+        errMessage: 'Missing parameters!',
+      });
+    }
+
+    try {
+      const userDetect = await User.findByIdAndRemove(req.body.id);
+      if (userDetect === null) {
+        res.status(200).json({
+          errCode: 1,
+          message: `The user is not exit!`,
+          userDetect,
+        });
+      }
+      res.status(200).json({
+        errCode: 0,
+        message: `The user is deleted`,
+        userDetect,
+      });
+    } catch (error) {
+      console.log(error);
+      next(createError(500, 'Some thing went wrong!'));
+    }
+  },
 };
 
 export default usersController;

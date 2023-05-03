c7a4e7570c0a551b081ca0932ebf6e8919143c01
Storm-cpu <lamchitinh1472002@gmail.com>
2023-05-03 10:16:35 +0700

    Designed home page backgournd header

--- client/src/containers/HomePage/HomeHeader.js
+++ client/src/containers/HomePage/HomeHeader.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+import {
+    faBars,faBedPulse,
+    faCircleQuestion,
+    faMagnifyingGlass,
+    faHospital,faBrain,
+    faMobile,faTooth,
+    faMicroscope
+} from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux';
 import "./HomeHeader.scss";
 class HomeHeader extends Component {
@@ -9,38 +16,80 @@ class HomeHeader extends Component {
 
 
         return (
-            <div className='home-header-container'>
-                <div className='home-header-content'>
-                    <div className='left-content'>
-                        <FontAwesomeIcon icon={faBars} />
-                        <div className='header-logo'></div>
-                    </div>
-                    <div className='center-content'>
-                        <div className='child-content'>
-                            <div><b>Chuyên khoa</b></div>
-                            <div className='sub-title'>Tìm bác sĩ theo chuyên khoa</div>
+            <React.Fragment>
+                <div className='home-header-container'>
+                    <div className='home-header-content'>
+                        <div className='left-content'>
+                            <FontAwesomeIcon icon={faBars} />
+                            <div className='header-logo'></div>
                         </div>
-                        <div className='child-content'>
-                            <div><b>Cơ sở y tế</b></div>
-                            <div className='sub-title'>Chọn bệnh viện phòng khám</div>
+                        <div className='center-content'>
+                            <div className='child-content'>
+                                <div><b>Chuyên khoa</b></div>
+                                <div className='sub-title'>Tìm bác sĩ theo chuyên khoa</div>
+                            </div>
+                            <div className='child-content'>
+                                <div><b>Cơ sở y tế</b></div>
+                                <div className='sub-title'>Chọn bệnh viện phòng khám</div>
+                            </div>
+                            <div className='child-content'>
+                                <div><b>Bác sĩ</b></div>
+                                <div className='sub-title'>Chọn bác sĩ giỏi</div>
+                            </div>
+                            <div className='child-content'>
+                                <div><b>Gói khám</b></div>
+                                <div className='sub-title'>Khám sức khỏe tổng quát</div>
+                            </div>
                         </div>
-                        <div className='child-content'>
-                            <div><b>Bác sĩ</b></div>
-                            <div className='sub-title'>Chọn bác sĩ giỏi</div>
+                        <div className='right-content'>
+                            <div className='support'>
+                                <FontAwesomeIcon icon={faCircleQuestion} /> Hỗ trợ
+                            </div>
+                            <div className='flag'>VN</div>
                         </div>
-                        <div className='child-content'>
-                            <div><b>Gói khám</b></div>
-                            <div className='sub-title'>Khám sức khỏe tổng quát</div>
-                        </div>
-                    </div>
-                    <div className='right-content'>
-                        <div className='support'>
-                            <FontAwesomeIcon icon={faCircleQuestion} /> Hỗ trợ
-                        </div>
-                        <div className='flag'>VN</div>
                     </div>
                 </div>
-            </div>
+                <div className='home-header-banner'>
+                    <div className='content-up'>
+                        <div className='title1'>NỀN TẢNG Y TẾ</div>
+                        <div className='title2'>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                        <div className='search'>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            <input type='text' />
+                        </div>
+                    </div>
+                    <div className='content-down'>
+                        <div className='options'>
+                            <div className='option-child'>
+                                <div className='icon-child'><i><FontAwesomeIcon icon={faHospital} /></i></div>
+                                <div className='text-child'>Khám chuyên khoa</div>
+                            </div>
+                            <div className='option-child'>
+                                <div className='icon-child'><i><FontAwesomeIcon icon={faMobile} /></i></div>
+                                <div className='text-child'>Khám từ xa</div>
+                            </div>
+                            <div className='option-child'>
+                                <div className='icon-child'><i><FontAwesomeIcon icon={faBedPulse} /></i></div>
+                                <div className='text-child'>Khám tổng quát</div>
+                            </div>
+                            <div className='option-child'>
+                                <div className='icon-child'><i><FontAwesomeIcon icon={faMicroscope} /></i></div>
+                                <div className='text-child'>Xét nghiệm y học</div>
+                            </div>
+                            <div className='option-child'>
+                                <div className='icon-child'><i><FontAwesomeIcon icon={faBrain} /></i></div>
+                                <div className='text-child'>Sức khỏe tinh thần</div>
+                            </div>
+                            <div className='option-child'>
+                                <div className='icon-child'><i><FontAwesomeIcon icon={faTooth} /></i></div>
+                                <div className='text-child'>Khám nha khoa</div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </React.Fragment>
         );
     }
 

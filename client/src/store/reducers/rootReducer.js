e092d07a96484b9867bfd91a2be94eb4c6f22295
Hoa Huu Nguyen <hoaboy89@gmail.com>
2023-05-10 18:52:02 +0700

    Identity role set header.

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,26 +1,33 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
 
 import appReducer from "./appReducer";
 import userReducer from "./userReducer";
 
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 const persistCommonConfig = {
-    storage: storage,
-    stateReconciler: autoMergeLevel2,
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
 };
 
 const userPersistConfig = {
-    ...persistCommonConfig,
-    key: 'user',
-    whitelist: ['isLoggedIn', 'userInfo']
+  ...persistCommonConfig,
+  key: "user",
+  whitelist: ["isLoggedIn", "userInfo"],
 };
 
-export default (history) => combineReducers({
+const appPersistConfig = {
+  ...persistCommonConfig,
+  key: "app",
+  whitelist: ["language"],
+};
+
+export default (history) =>
+  combineReducers({
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
-    app: appReducer
-})
+    app: persistReducer(appPersistConfig, appReducer),
+  });
--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,27 +1,39 @@
 export const path = {
-    HOME: '/',
-    HOMEPAGE: '/home',
-    LOGIN: '/login',
-    LOG_OUT: '/logout',
-    SYSTEM: '/system'
+  HOME: "/",
+  HOMEPAGE: "/home",
+  LOGIN: "/login",
+  LOG_OUT: "/logout",
+  SYSTEM: "/system",
+  DETAIL_DOCTOR: "/detail-doctor/:id",
+  DETAIL_SPCIALTY: "/detail-specialty/:id",
+  DETAIL_CLINIC: "/detail-clinic/:id",
+
+  VERIFY_EMAIL_BOOKING: "/verify-booking",
 };
 
 export const LANGUAGES = {
-    VI: 'vi',
-    EN: 'en'
+  VI: "vi",
+  EN: "en",
 };
 
-export const manageActions = {
-    ADD: "ADD",
-    EDIT: "EDIT",
-    DELETE: "DELETE"
+export const CRUD_ACTIONS = {
+  CREATE: "CREATE",
+  EDIT: "EDIT",
+  DELETE: "DELETE",
+  READ: "READ",
 };
 
 export const dateFormat = {
-    SEND_TO_SERVER: 'DD/MM/YYYY'
+  SEND_TO_SERVER: "DD/MM/YYYY",
 };
 
 export const YesNoObj = {
-    YES: 'Y',
-    NO: 'N'
-}
+  YES: "Y",
+  NO: "N",
+};
+
+export const USER_ROLE = {
+  ADMIN: "R1",
+  DOCTOR: "R2",
+  PATIENT: "R3",
+};

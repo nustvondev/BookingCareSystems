4ced394c7cd3612b818a10fc5f03b3473046d0e0
Hoa Huu Nguyen <hoaboy89@gmail.com>
2023-05-15 22:29:50 +0700

    Down version react 18 to 17.0.2.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/styles.scss";
 
@@ -10,16 +10,18 @@ import IntlProviderWrapper from "./hoc/IntlProviderWrapper";
 import { Provider } from "react-redux";
 import reduxStore, { persistor } from "./redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const renderApp = () => {
+  ReactDOM.render(
+    <Provider store={reduxStore}>
+      <IntlProviderWrapper>
+        <App persistor={persistor} />
+      </IntlProviderWrapper>
+    </Provider>,
+    document.getElementById("root")
+  );
+};
 
-root.render(
-  <Provider store={reduxStore}>
-    <IntlProviderWrapper>
-      <App persistor={persistor} />
-    </IntlProviderWrapper>
-  </Provider>
-);
-
+renderApp();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
